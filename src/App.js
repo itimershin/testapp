8736ef5d50ee0f090859feecbbb3fc07f78651c1
root <root@timershin.ildar.fvds.ru>
2021-07-27 11:52:03 +0300

    sadfsdfds

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learfdfd
+ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd          Learfdfd
 dfd
 df
 df
